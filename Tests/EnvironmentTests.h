287e44fc2daa744b6c8cffddf6f7e6c57f79208e
Calder Coalson <caldercoalson@gmail.com>
2012-10-26 15:03:21 -0500

    Cleaned up EnvironmentTests.

--- Tests/EnvironmentTests.h
+++ Tests/EnvironmentTests.h
@@ -5,24 +5,19 @@
 #include "TestUtil.h"
 
 
+void environment_set_int (Environment* environment, const char* symbol, int value);
+void environment_check_int (Environment* environment, const char* symbol, int value);
+
+
 void test_environment_overwrite ()
 {
     start_test("Environment - Overwrite");
     Environment* environment = environment_create(NULL);
-    Value* value;
 
-    value = value_create(FLOAT_VALUE);
-    value->floatVal = 1.23;
-    environment_set(environment, "x", value);
-    value_release(value);
-
-    value = value_create(INTEGER_VALUE);
-    value->intVal = 456;
-    environment_set(environment, "x", value);
-    value_release(value);
-
-    value = environment_get(environment, "x");
-    assert(value->intVal == 456);
+    environment_set_int(environment, "x", 123);
+    environment_check_int(environment, "x", 123);
+    environment_set_int(environment, "x", 456);
+    environment_check_int(environment, "x", 456);
 
     environment_release(environment);
 }
@@ -33,27 +28,13 @@ void test_environment_shadow ()
     start_test("Environment - Shadow");
     Environment* environment1 = environment_create(NULL);
     Environment* environment2 = environment_create(environment1);
-    Value* value;
 
-    // Set and check original value
-    value = value_create(INTEGER_VALUE);
-    value->intVal = 314;
-    environment_set(environment1, "x", value);
-    value_release(value);
-
-    // Check original value
-    value = environment_get(environment2, "x");
-    assert(value->intVal == 314);
-
-    /// Set shadowed value
-    value = value_create(INTEGER_VALUE);
-    value->intVal = 789;
-    environment_set(environment2, "x", value);
-    value_release(value);
-
-    // Check shadowed value
-    value = environment_get(environment2, "x");
-    assert(value->intVal == 789);
+    environment_set_int(environment1, "x", 123);
+    environment_check_int(environment1, "x", 123);
+    environment_check_int(environment2, "x", 123);
+    environment_set_int(environment2, "x", 456);
+    environment_check_int(environment1, "x", 123);
+    environment_check_int(environment2, "x", 456);
 
     environment_release(environment1);
     environment_release(environment2);
@@ -64,6 +45,23 @@ void test_environment ()
 {
     test_environment_overwrite();
     test_environment_shadow();
+}
+
+
+void environment_set_int (Environment* environment, const char* symbol, int intVal)
+{
+    Value* value = value_create(INTEGER_VALUE);
+    value->intVal = intVal;
+    environment_set(environment, symbol, value);
+    value_release(value);
+}
+
+
+void environment_check_int (Environment* environment, const char* symbol, int intVal)
+{
+    Value* value = environment_get(environment,symbol);
+    assert(value->type == INTEGER_VALUE);
+    assert(value->intVal == intVal);
 }
 
 
