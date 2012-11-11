5c82f170b3e0fb138c50fe76bb8de0224e168ed6
Maegereg <maegereg@gmail.com>
2012-11-10 20:49:52 -0600

    Added all integer comparison functions (= > >= < <=).

--- Library/Interpreter/Functions.c
+++ Library/Interpreter/Functions.c
@@ -246,6 +246,18 @@ Value* function_divide (Environment* environment, ParseTree* args)
 }
 
 
+Value* function_greaterthen (Environment* environment, ParseTree* args)
+{
+    return function_comparator(environment, args, 2);
+}
+
+
+Value* function_greatertheneqto (Environment* environment, ParseTree* args)
+{
+    return function_comparator(environment, args, 4);
+}
+
+
 Value* function_if (Environment* environment, ParseTree* args)
 {
     // Check number of arguments
@@ -288,9 +300,15 @@ Value* function_lambda (Environment* environment, ParseTree* args)
 }
 
 
-Value* function_lessthan (Environment* environment, ParseTree* args)
+Value* function_lessthen (Environment* environment, ParseTree* args)
 {
     return function_comparator(environment, args, 1);
+}
+
+
+Value* function_lesstheneqto (Environment* environment, ParseTree* args)
+{
+    return function_comparator(environment, args, 3);
 }
 
 
@@ -451,6 +469,12 @@ Value* function_null (Environment* environment, ParseTree* args)
         { null = true; }
     value_release(value);
     return value_create_bool(null);
+}
+
+
+Value* function_numequals (Environment* environment, ParseTree* args)
+{
+    return function_comparator(environment, args, 0);
 }
 
 
